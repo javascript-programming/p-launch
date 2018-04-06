9aba0e520ca69195404a0765983058332b78b01b
terencevanzoelen <terence@jaav.nl>
2018-04-06 17:42:16 +0200

    Pensioen opbouw excel + utils

--- contracts/PrePensionLib.sol
+++ contracts/PrePensionLib.sol
@@ -2,19 +2,27 @@ pragma solidity ^0.4.18;
 
 library PrePensionLib {
 
-    uint totalBalance;
-
     struct Participant {
+        string id;
+        uint balance;
+        mapping(string => )
+    }
+
+    struct Supplier {
         string id;
         uint balance;
     }
 
+    struct PensionFund {
+        string id;
+        uint minted;
+    }
+
     struct Data {
-        mapping(uint => Task) tasks;
-        mapping(address => Resource) resources;
-        mapping(uint => Assignment) assignments;
-        uint p_counter;
-        uint budget;
+        mapping(string => Participant) participants;
+        mapping(string => Supplier) suppliers;
+        mapping(string => PensionFund) pensionFunds;
+        uint mintedCoins;
     }
 
 }
