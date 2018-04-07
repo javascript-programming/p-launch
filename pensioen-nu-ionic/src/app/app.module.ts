7803094e7e59ea66af1b88d4b02b31f9e1661b10
Cookie <ward@studioanderekoek.com>
2018-04-07 18:02:04 +0200

    startup sound

--- pensioen-nu-ionic/src/app/app.module.ts
+++ pensioen-nu-ionic/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { SmartAudioProvider } from '../providers/smart-audio/smart-audio';
+import { NativeAudio } from '@ionic-native/native-audio';
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { TestPage } from '../pages/test/test';
@@ -29,6 +33,8 @@ import { Web3Service } from '../providers/web3/web3.service';
   providers: [
     StatusBar,
     SplashScreen,
+    SmartAudioProvider,
+    NativeAudio,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
       Web3Service
   ]
--- pensioen-nu-ionic/src/pages/home/home.ts
+++ pensioen-nu-ionic/src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SmartAudioProvider } from '../../providers/smart-audio/smart-audio';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      public smartAudioProvider: SmartAudioProvider
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  ionViewDidEnter() {
+
+    setTimeout(() => {
+      this.smartAudioProvider.play('splash');
+    }, 0);
+    //
+    // setTimeout(() => {
+    //   this.viewCtrl.dismiss();
+    // }, 4000);
 
   }
 
 }
+
