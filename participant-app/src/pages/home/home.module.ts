3cb2ab7bc4cd7ac9ea8187ebd6559451293b2756
Cookie <ward@studioanderekoek.com>
2018-04-08 00:04:30 +0200

    remove import

--- participant-app/src/pages/home/home.module.ts
+++ participant-app/src/pages/home/home.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
-import { LogoModule } from '../../components/logo/logo.module'
 
 
 @NgModule({
@@ -9,8 +8,7 @@ import { LogoModule } from '../../components/logo/logo.module'
     HomePage,
   ],
   imports: [
-    IonicPageModule.forChild(HomePage),
-    LogoModule
+    IonicPageModule.forChild(HomePage)
   ],
 })
 export class HomePageModule {}
